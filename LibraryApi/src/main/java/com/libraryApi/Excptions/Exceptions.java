42f1418d0c449194d27eb92fe2a0833bc4dad0ad
omedeiroz <contato.medeirosart@gmail.com>
2025-03-21 18:46:30 -0300

    fix: book dto

---
+++ LibraryApi/src/main/java/com/libraryApi/Excptions/Exceptions.java
@@ -0,0 +1,7 @@
+package com.libraryApi.Excptions;
+
+public class Exceptions extends RuntimeException {
+    public Exceptions(String message) {
+        super(message);
+    }
+}
--- LibraryApi/src/main/java/com/libraryApi/dto/BookDTO.java
+++ LibraryApi/src/main/java/com/libraryApi/dto/BookDTO.java
@@ -1,25 +1,5 @@
 package com.libraryApi.dto;
-
-
 import com.libraryApi.Enum.BookGenre;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class BookDTO {
-
-    private Long id;
-    private String title;
-    private String authors;
-    private String edition;
-    private BookGenre genre;
-
-    public BookDTO(Long id, String title, String authors, String edition, BookGenre genre) {
-        this.id = id;
-        this.title = title;
-        this.authors = authors;
-        this.edition = edition;
-        this.genre = genre;
-    }
+public record BookDTO(Long id, String title, String authors, String edition, BookGenre genre ) {
 }
